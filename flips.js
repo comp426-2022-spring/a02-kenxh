b91f12ab9757e870f07a13d0d8fec052d56f1888
Kendall Howard <69732946+kenxh@users.noreply.github.com>
2022-02-11 16:52:24 -0500

    Testing no argument

--- flips.js
+++ flips.js
@@ -6,6 +6,6 @@ const require = createRequire(import.meta.url);
 const args = require('minimist')(process.argv.slice(2));
 
 let flips = coinFlips(args.number)
-
+console.log(args.number)
 console.log(flips)
 console.log(countFlips(flips))
